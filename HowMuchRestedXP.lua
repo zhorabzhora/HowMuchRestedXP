8e491aeb5a0db6119e88be9587f75dc9143f6d58
zhora <wither02@gmail.com>
2022-10-12 18:45:43 +0300

    added checking for zero rested xp added function to calculate how many seconds left to get fully rested, count is the number of tents

--- HowMuchRestedXP.lua
+++ HowMuchRestedXP.lua
@@ -6,7 +6,12 @@ local function HowMuchRestedXP(count)
         local playerName = UnitName("player")
 		local XPExhaustion = GetXPExhaustion()
 		local XPMax = UnitXPMax("player")
-		DEFAULT_CHAT_FRAME:AddMessage("You have " .. XPExhaustion .. " rested XP " .. "(" ..  math.ceil(XPExhaustion * 100 / XPMax) .. '%' .. ")")
+		if XPExhaustion == nul then XPExhaustion = 0 end
+		if count == "" then
+			DEFAULT_CHAT_FRAME:AddMessage("You have " .. XPExhaustion .. " rested XP " .. "(" ..  math.ceil(XPExhaustion * 100 / XPMax) .. '%' .. ")")
+		else
+			DEFAULT_CHAT_FRAME:AddMessage("You will get 150% rested xp in about " .. ((XPMax * 1.5) - XPExhaustion) / (76 * count) .. " seconds")
+		end
 end
 
 
